64a923f83a0045d47e8f2c4c1962da5969b04069
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-06-09 18:07:53 +0200

    Crud: Delete.

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,16 +1,30 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+
+    @if (session('message'))
+    <div class="alert alert-success m-4" role="alert">
+
+        {{ session('message') }}
+
+    </div>
+    @endif
+
     @forelse ($posts as $post)
     <div class="card text-center m-4">
         <div class="card-header">
-            
+
             <h5 class="card-title text-capitalize m-0">{{ $post->title }}</h5>
         </div>
         <div class="card-body">
             <img class="mb-3 rounded" width="50%" src="{{$post->image}}" alt="" srcset="">
             <p class="card-text">{{ $post->content }}</p>
             <a href="{{ route('admin.posts.show', $post->id ) }}" class="btn btn-primary">Details</a>
+            <form class="d-inline" action="{{ route('admin.posts.destroy', $post->id ) }}" method="POST">
+                @method('Delete')
+                @csrf
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
         </div>
         <div class="card-footer text-muted">
             {{ $post->created_at }}
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    
+    @if (session('message'))
+    <div class="alert alert-success m-4" role="alert">
+
+        {{ session('message') }}
+
+    </div>
+    @endif
     <div class="card text-center m-4">
         <div class="card-header">
             
@@ -10,6 +16,11 @@
         <div class="card-body">
             <img class="mb-3 rounded" width="50%" src="{{$post->image}}" alt="" srcset="">
             <p class="card-text">{{ $post->content }}</p>
+            <form class="d-inline" action="{{ route('admin.posts.destroy', $post->id ) }}" method="POST">
+                @method('Delete')
+                @csrf
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
            
         </div>
         <div class="card-footer text-muted">
