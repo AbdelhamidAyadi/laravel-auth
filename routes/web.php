f4e9a51c88caa5c7929393c85e5f51e7ed8a8735
AbdelhamidAyadi <Abdelhamid2002ayadi@gmail.com>
2022-06-09 15:46:21 +0200

    Commit.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+->prefix('admin')
+->namespace('Admin')
+->name('admin.')
+->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts','PostController');
+});
+
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where("any" , ".*" );
